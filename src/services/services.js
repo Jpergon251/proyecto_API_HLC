5ba32765755ea64ef118d0e42139e7da2e506692
Jpergon251 <Jpergon251@g.educaand.es>
2023-03-06 12:20:36 +0100

    terminado

--- src/services/services.js
+++ src/services/services.js
@@ -1,5 +1,4 @@
 const modelos = require("../database/modelos");
-const { v1: uuid } = require("uuid");
 
 //**** Servicios Jugadores ****
 
@@ -8,16 +7,7 @@ const getJugadores = () => {
     const jugadores = modelos.getJugadores();
     return jugadores;
   };
-const postJugador= (nombre,edad) => {
-
-  var lowerLimit = 500;
-  var upperLimit = 10000;
-  var range = upperLimit - lowerLimit;
-
-  var randomNumber = Math.floor(Math.random() * (range + 1)) + lowerLimit;
-
-  const horasJugadas = randomNumber;
-
+const postJugador= (nombre,edad,horasJugadas) => {
   const newJugador = {
     nombre,
     edad,
@@ -51,40 +41,11 @@ const getPartida = (id) => {
   const partida = modelos.getPartida(id)
   return partida 
 }
-const postPartida = () => {
-  var lowerLimit = 600;
-  var upperLimit = 3600;
-  var range = upperLimit - lowerLimit;
-
-  var randomNumber = Math.floor(Math.random() * (range + 1)) + lowerLimit;
-
-  const duracion = randomNumber
+const postPartida = (juego,
+  fecha,
+  victoria,
+  duracion,) => {
   
-  const jugadores =modelos.getJugadores()
-
-  const matriz = Object.values(jugadores)
-  
-  const seleccion = matriz.sort(() => Math.random() - 0.5).slice(0, 6);
-  
-  const jugadoresPartida = {};
-
-  
-  for (let i = 0; i < seleccion.length; i++) {
-    jugadoresPartida[`${i+1}`] = seleccion[i];
-  }
-  
-  
-  const juegos = modelos.getJuegos();
-  const claves = Object.keys(juegos);
-
-  const indiceAleatorio = Math.floor(Math.random() * claves.length);
-  const claveAleatoria = claves[indiceAleatorio];
-  const juego = juegos[claveAleatoria];
-
-
-  const idPartida = uuid()
-  const fecha = new Date().toLocaleDateString()
-  const victoria = Math.random() < 0.5;
 
   const newPartida = {
     idPartida,
